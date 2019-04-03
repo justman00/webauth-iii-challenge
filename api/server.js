01dbd34cd3eefb3146a42a3852d2cacedcb0699e
Vlad <vovaturcan4@gmail.com>
2019-04-03 23:30:29 +0300

    finished mvp

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const authRouter = require("../auth/auth-router");
+const authMiddleware = require("./auth-middleware");
+const usersRouter = require("../users/users-router");
 
 const server = express();
 
 server.use(express.json());
 
 server.use("/api/auth", authRouter);
+server.use("/api", authMiddleware, usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h1>Hello World</h1>`);
